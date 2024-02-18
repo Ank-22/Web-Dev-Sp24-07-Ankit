b77ec517f015dd75f3f51dde5730d8586ad544f9
Ankit Sinha <40303711+Ank-22@users.noreply.github.com>
2024-02-17 21:20:05 -0500

    2.1 Completed ✅

---
+++ src/Labs/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Assignment3 from "./a3";
+
+function Labs() {
+    return (
+       <div>
+          <Assignment3 />
+       </div>
+    );
+ }
+ export default Labs;
