655f2403c7bc93d7194966521528e905a34af7ad
Ankit Sinha <40303711+Ank-22@users.noreply.github.com>
2024-02-18 10:13:13 -0500

    2.2.8 Completed 🕵️

--- src/Labs/a3/JavaScript/index.tsx
+++ src/Labs/a3/JavaScript/index.tsx
@@ -7,6 +7,15 @@ import ES5Functions from "./conditionals/ES5Functions";
 import ArrowFunctions from "./conditionals/ArrowFunctions";
 import ImpliedReturn from "./conditionals/ImpliedReturn";
 import FunctionParenthesisAndParameters from "./conditionals/FunctionParenthesisAndParameters";
+import ArrayIndexAndLength from "./arrays/ArrayIndexAndLength";
+import AddingAndRemovingDataToFromArrays from "./arrays/AddingAndRemovingDataToFromArrays";
+import ForLoops from "./arrays/ForLoops";
+import MapFunction from "./arrays/MapFunction";
+import JsonStringify from "./arrays/JsonStringify";
+import FindFunction from "./arrays/FindFunction";
+import FindIndex from "./arrays/FindIndex";
+import FilterFunction from "./arrays/FilterFunction";
+import TemplateLiterals from "./arrays/TemplateLiterals";
 function JavaScript() {
   console.log("Hello World");
   return (
@@ -21,6 +30,15 @@ function JavaScript() {
       <ArrowFunctions />
       <ImpliedReturn />
       <FunctionParenthesisAndParameters />
+      <ArrayIndexAndLength />
+      <AddingAndRemovingDataToFromArrays />
+      <ForLoops />
+      <MapFunction />
+      <JsonStringify />
+      <FindFunction />
+      <FindIndex />
+      <FilterFunction />
+      <TemplateLiterals />
     </div>
   );
 }
