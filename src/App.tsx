10e41d433189b1b587e7ca3b98a02783cf080a3e
Ankit Sinha <40303711+Ank-22@users.noreply.github.com>
2024-02-18 09:16:19 -0500

    2.2.5 Done ✅

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import Labs from "./Labs";
 import HelloWorld from './Labs/a3/HelloWorld';
-import Kanbas from './Labs/Kanbas';
+import Kanbas from './Kanbas';
 
 function App() {
   return (
     <div>
     <Labs />
-    <Kanbas/>
+    <Kanbas />
     <HelloWorld />
     </div>
 );
