eec409fb502f24070e7645ba412c21f249024548
Ankit Sinha <40303711+Ank-22@users.noreply.github.com>
2024-02-19 11:55:37 -0500

    Lab Done🥳

--- src/Labs/a3/index.tsx
+++ src/Labs/a3/index.tsx
@@ -3,6 +3,9 @@ import PathParameters from "./routing/PathParameters";
 import Classes from "./Classes";
 import Styles from "./Styles";
 import ConditionalOutput from "./ConditionalOutput";
+import Highlight from "./Highlight";
+import Add from "./add";
+import TodoList from "./todos/TodoList";
 
 
 function Assignment3() {
@@ -14,6 +17,12 @@ function Assignment3() {
         <Classes />
         <JavaScript />
         <PathParameters />
+        <Highlight>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+        vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+     </Highlight>
+     <Add a={3} b={4} />
+      <TodoList />
       </div>
     );
   }
