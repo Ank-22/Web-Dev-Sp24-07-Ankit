66043d3a54ac6e9a96d0be5304c2a02df74f410a
Ankit Sinha <40303711+Ank-22@users.noreply.github.com>
2024-02-19 07:39:32 -0500

    2.7 Completed ✅

--- src/Labs/a3/HelloWorld.tsx
+++ src/Labs/a3/HelloWorld.tsx
@@ -1,4 +1,10 @@
-function HelloWorld(){
-    return <h1>Hello World!</h1>;
-};
+import Nav from "../../Nav";
+function HelloWorld() {
+ return (
+   <div>
+     <Nav/>
+     <h1>Hello World!</h1>
+   </div>
+ );
+}
 export default HelloWorld;
