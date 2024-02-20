2db1808838bb79051d5a6d12503faa45d7d28c75
Ankit Sinha <40303711+Ank-22@users.noreply.github.com>
2024-02-20 17:33:32 -0500

    Kanbas Navigation Done

---
+++ src/Kanbas/Navigation/index.tsx
@@ -0,0 +1,33 @@
+import { Link, useLocation } from "react-router-dom";
+import "./index.css";
+import { FaTachometerAlt, FaRegUserCircle, FaBook, FaRegCalendarAlt, FaInbox, FaRegClock, FaCreativeCommonsSa} from "react-icons/fa";
+import { Fa6, FaDisplay, FaQuestion } from "react-icons/fa6";
+function KanbasNavigation() {
+  const links = [
+    { label: "Account",   icon: <FaRegUserCircle className="fs-2" />  },
+    { label: "Dashboard", icon: <FaTachometerAlt className="fs-2" />  },
+    { label: "Courses",   icon: <FaBook className="fs-2" />           },
+    { label: "Calendar",  icon: <FaRegCalendarAlt className="fs-2" /> },
+    { label: "Inbox",  icon: <FaInbox className="fs-2" /> },
+    { label: "History",  icon: <FaRegClock className="fs-2" /> },
+    { label: "Studio",  icon: <FaDisplay className="fs-2" /> },
+    { label: "Commons",  icon: <FaCreativeCommonsSa className="fs-2" /> },
+    { label: "Help",  icon: <FaQuestion className="fs-2" /> },
+
+  ];
+  const { pathname } = useLocation();
+  return (
+    <ul className="wd-kanbas-navigation">
+       <li>
+       <Link to={`/Kanbas/`}><img width="95%" height="6.5%" src="/images/Logo.png" /></Link></li>
+       {links.map((link, index) => (
+        <li key={index} className={pathname.includes(link.label) ? "wd-active" : ""}>
+          <Link to={`/Kanbas/${link.label}`} style={{ color: (!pathname.includes(link.label) && link.label === "Account") ? "white" : "" }}>
+            {link.icon} {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+export default KanbasNavigation;
