ea1e754a55ccc7d9fc7c7eddafe6df3e93935c8f
Ankit Sinha <40303711+Ank-22@users.noreply.github.com>
2024-03-11 12:21:58 -0400

    A4 - 2.2- Done 🐲

--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -1,7 +1,19 @@
+import ClickEvent from "./ClickEvent";
+import EventObject from "./EventObject";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+
 function Assignment4() {
+  function sayHello() {
+    alert("Hello");
+  }
     return (
       <div className="container">
         <h1>Assignment 4</h1>
+        <ClickEvent/>
+        <PassingDataOnEvent/>
+        <PassingFunctions theFunction={sayHello} />
+        <EventObject/>
       </div>
     );
   }
